36b5c620639ac321db50a1cc5fc47ef027f4f4ab
shivam-0806 <79564534+shivam-0806@users.noreply.github.com>
2024-08-16 19:16:58 +0530

    Add files via upload

---
+++ Task As3.py
@@ -0,0 +1,7 @@
+M = 1.989*(10**30) #given
+a = 1.5*(10**11) #given
+G = 6.674*(10**(-11)) #given
+
+T = ((4*(3.14**2)*(a**3)/(G*M))**0.5)/31536000 #apply formula in terms of years
+
+print(f"Orbital period: {T} years")
